ef714f978c098cf435482d6a48a6d76f692fc3db
Aylin526 <l23200857@pachuca.tecnm.mx>
2026-04-15 17:10:22 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ function createParticles(x, y) {
 // =============================
 // 🔹 DIFICULTAD
 // =============================
-let baseSpeed = 0.1;
+let baseSpeed = 0.05;
 
 // =============================
 // 🔹 BALÓN
